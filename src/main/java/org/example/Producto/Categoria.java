1042a82e035c048aca658600f290c109a822bb69
Maria <mapis2321@gmail.com>
2023-10-25 18:31:45 -0500

    Se agregó el tipo común Enum a la clase Categoria

--- src/main/java/org/example/Producto/Categoria.java
+++ src/main/java/org/example/Producto/Categoria.java
@@ -3,6 +3,7 @@ package org.example.Producto;
 public abstract class  Categoria {
     public abstract void listar();
     enum Cate{
+        //Categorias Unicas
         AseoHogar,Bebidas,CarnesFriasyCongelados,Cigarrillos,CuidadoBebe,CuidadoPersonal,CuidadoRopa,Despensa,
         Drogueria,DulcesyPostres,Electrodomesticos,Helados,HogaryDecoración,IluminacionyElectricos,
         LacteosHuevosyRefrigerados,LimpiezaCocina,Mascotas,PanaderiayPasteleria,Pasabocas,ProductosCongelados,
